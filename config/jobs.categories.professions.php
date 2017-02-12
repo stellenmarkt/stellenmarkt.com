b6575ecb3a0a28bd2f402ac98f9320aafff5513b
Carsten Bleek <bleek@cross-solution.de>
2017-02-12 18:57:20 +0100

    updates CSS

---
+++ config/jobs.categories.professions.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2017 Cross Solution <http://cross-solution.de>
+ */
+
+/*
+ * Format:
+ * [
+ *      'name' => <name>, [required]
+ *      'value' => <value>, [optional]
+ *      'children' => [ // optional
+ *          <name> // strings will be treated as ['name' => <name>]
+ *          [
+ *              'name' => <name>, [required]
+ *              'value' => <value>, [optional]
+ *              'children' => [ ... ]
+ *       ]
+ * ]
+ */
+
+return [
+    'name' => 'Professions',
+    'children' => [
+        'Barkeeper/-in',
+        'Kellner/-in',
+        'Koch/Köchin',
+        'Küchenchef/-in',
+        'Hotelfachmann/-frau',
+        'Hoteldirektor/-in',
+        'Rezeptionist/-in',
+        'Restaurantfachmann/-frau',
+        ]
+];
+
