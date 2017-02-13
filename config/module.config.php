343b6126cb19bc05b47b1da77f8473c6c02b40b3
Carsten Bleek <bleek@cross-solution.de>
2017-02-13 11:35:27 +0100

    Löscht die Kategorien im Template

--- config/module.config.php
+++ config/module.config.php
@@ -19,7 +19,6 @@ return array(
                      'layout/application-form' => __DIR__ . '/../view/application-form.phtml',
                      'core/index/index' => __DIR__ . '/../view/index.phtml',
                      'piwik' => __DIR__ . '/../view/piwik.phtml',
-                     'iframe/iFrame.phtml'               => __DIR__ . '/../view/iFrame.phtml',
                      'jobs/jobboard/index.ajax.phtml' => __DIR__ . '/../view/jobs/index.ajax.phtml',
                      'auth/users/list.ajax.phtml' => __DIR__ . '/../view/auth/users/list.ajax.phtml', // hide email adresses, since this is is a public demo
                       ),
@@ -37,11 +36,7 @@ return array(
 
              'form_elements' => [
                  'invokables' => [
-                     'Gastro24/ClassificationForm' => 'Gastro24\Form\ClassificationForm',
                      'Jobs/Description' => 'Gastro24\Form\JobsDescription',
-                 ],
-                 'factories' => [
-                     'Gastro24/ClassificationFieldset' => 'Gastro24\Form\ClassificationFieldsetFactory',
                  ],
              ],
     'router'       => array(
