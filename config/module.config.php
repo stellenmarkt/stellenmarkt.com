6b484acb847dfb9969429142421a1f839e0b204c
Carsten Bleek <bleek@cross-solution.de>
2017-03-16 17:15:31 +0100

    map wordpress page 2=>koch and 6=>hotelfachmann (as an example). Avoids accessing attributes directly. Will be deprecated in 0.29

--- config/module.config.php
+++ config/module.config.php
@@ -25,7 +25,6 @@ return [
                      'jobs/jobboard/index' => __DIR__ . '/../view/jobs/index.phtml',
                      'main-navigation' => __DIR__ . '/../view/main-navigation.phtml',
                      'auth/index/login-info' => __DIR__ . '/../view/login-info.phtml',
-                     'auth/users/list.ajax.phtml' => __DIR__ . '/../view/auth/users/list.ajax.phtml', // hide email adresses, since this is is a public demo
                       ],
              ],
              'translator' => [
