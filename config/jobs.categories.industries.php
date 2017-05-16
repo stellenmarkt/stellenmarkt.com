45222dbc4cc35d7072cdc2bd3f9755d333826209
Carsten Bleek <bleek@cross-solution.de>
2017-05-16 12:25:18 +0200

    aktualisiert Berufsfelder

---
+++ config/jobs.categories.industries.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2017 Cross Solution <http://cross-solution.de>
+ */
+
+/*
+ * Format:
+ * [
+ *      'name' => <name>, [required]
+ *      'value' => <value>, [optional]
+ *      'children' => [ // optional
+ *          <name> // strings will be treated as ['name' => <name>]
+ *          [
+ *              'name' => <name>, [required]
+ *              'value' => <value>, [optional]
+ *              'children' => [ ... ]
+ *       ]
+ * ]
+ */
+
+return [
+    'name' => 'Industries',
+    'children' => [
+        'Hotel (4 Sterne)',
+        'Krankenhaus',
+        'Schiff',
+        'Kantine'
+        ]
+];
+
