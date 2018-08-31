fb3f1803dc23cd10d4c88f50d830a51630d6f2b9
Mathias Gelhausen <gelhausen@cross-solution.de>
2018-08-31 12:35:59 +0200

    Bewerbungen: Button 'Verschieben' ausblenden (#206)

--- config/module.config.php
+++ config/module.config.php
@@ -150,6 +150,9 @@ return [
                 [\Zend\View\Helper\HeadScript::SCRIPT, ';(function($) { $(function() { $("#sf-general-portalForm").hide(); }); })(jQuery);'],
             ],
             [\Zend\View\Helper\HeadScript::SCRIPT, ';(function($) { $(function() { $("#jobs-list-filter").find("button[type=\'reset\']").text("X"); }); })(jQuery);'],
+            'lang/applications/detail' => [
+                [\Zend\View\Helper\HeadScript::SCRIPT, ';(function($) { $(function() { $("button[data-target=\'#cam-move-application\']").hide(); }); })(jQuery);'],
+            ],
         ],
     ],
 
