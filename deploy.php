e91273683f82f9991ce78a9d00d1d5eb08443ec3
Carsten Bleek <bleek@cross-solution.de>
2019-05-27 15:03:20 +0200

    adds public/static to the deployment

--- deploy.php
+++ deploy.php
@@ -19,12 +19,14 @@ add('shared_dirs', [
     'test/sandbox/var/log',
     'test/sandbox/var/cache',
     'test/sandbox/config/autoload',
+    'test/sandbox/public/static',	// used by eg. organization logos
 ]);
 
 // Writable dirs by web server 
 add('writable_dirs', [
     'test/sandbox/var/cache',
     'test/sandbox/var/log',
+    'test/sandbox/public/static',
 ]);
 
 set('default_stage', 'prod');
